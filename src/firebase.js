95b49378dc4f55d08b61bfa1e3f6f986009740a6
Fade <fadepublicdvd@gmail.com>
2023-03-20 22:00:21 +0200

    initial committ

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBtMEf4azE45OPbWWnqWAkPcwLv3_f612s",
+  authDomain: "netflix-clone-ad47a.firebaseapp.com",
+  projectId: "netflix-clone-ad47a",
+  storageBucket: "netflix-clone-ad47a.appspot.com",
+  messagingSenderId: "789319452547",
+  appId: "1:789319452547:web:acc22b320fd731ea7798b6",
+};
