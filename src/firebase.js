e763abb81a6b666d6be99f0e1b7e84153e121fac
Fade <fadepublicdvd@gmail.com>
2023-03-21 03:36:40 +0200

    finished main

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBtMEf4azE45OPbWWnqWAkPcwLv3_f612s",
   authDomain: "netflix-clone-ad47a.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
   messagingSenderId: "789319452547",
   appId: "1:789319452547:web:acc22b320fd731ea7798b6",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
